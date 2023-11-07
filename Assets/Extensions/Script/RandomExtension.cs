c8731e74c9fe42300338693108e59592d00b211e
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2023-11-07 21:25:53 +0530

    Update getRandomItems

--- Assets/Extensions/Script/RandomExtension.cs
+++ Assets/Extensions/Script/RandomExtension.cs
@@ -17,9 +17,10 @@ namespace Extension
         /// <param name="list">The List of items from which to select randomly.</param>
         /// <param name="count">The number of random items to retrieve from the List.</param>
         /// <returns>A List of random items, containing 'count' elements, randomly selected from the input List.</returns>
-        public static List<T> GetRandomItems<T>(this List<T> list, int count = 1)
+        public static List<T> GetRandomItems<T>(this List<T> items, int count = 1)
         {
             System.Random random = new();
+            List<T> list = items.ToList();
             int n = list.Count; 
             for (int i = n - 1; i > 0; i--)
             {
@@ -27,6 +28,25 @@ namespace Extension
                 (list[i], list[j]) = (list[j], list[i]);
             }
             return list.Take(count).ToList();
+        }
+        /// <summary>
+        /// Retrieves a random selection of items from the  array.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the array.</typeparam>
+        /// <param name="items">The array  of items from which to select randomly.</param>
+        /// <param name="count">The number of random items to retrieve from the array.</param>
+        /// <returns>A array of random items, containing 'count' elements, randomly selected from the input array.</returns>
+        public static T[] GetRandomItems<T>(this T[] items, int count = 1)
+        {
+            System.Random random = new();
+            List<T> list = items.ToList();
+            int n = list.Count;
+            for (int i = n - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                (list[i], list[j]) = (list[j], list[i]);
+            }
+            return list.Take(count).ToArray();
         }
 
         /// <summary>
