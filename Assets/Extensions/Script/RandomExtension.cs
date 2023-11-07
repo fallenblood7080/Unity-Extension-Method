73aee1fdd226f197bdb3b6da7e4feb43f22801a7
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2023-11-07 21:17:44 +0530

    Added two more random func to get random point in circle and sphere

--- Assets/Extensions/Script/RandomExtension.cs
+++ Assets/Extensions/Script/RandomExtension.cs
@@ -28,5 +28,39 @@ namespace Extension
             }
             return list.Take(count).ToList();
         }
+
+        /// <summary>
+        /// Generates a random 2D point within a circular area defined by a center and radius.
+        /// </summary>
+        /// <param name="center">The center of the circle in 2D space.</param>
+        /// <param name="radius">The radius of the circle, determining the distance from the center to the generated point.</param>
+        /// <returns>A randomly generated Vector2 point within the specified circle.</returns>
+        public static Vector2 GetRandomPointInCircle(this Vector2 center, float radius)
+        {
+            float randomAngle = Random.Range(0f, 2f * Mathf.PI);
+            float randomDistance = Random.Range(0f, radius); 
+            float x = center.x + randomDistance * Mathf.Cos(randomAngle);
+            float y = center.y + randomDistance * Mathf.Sin(randomAngle);
+            return new(x,y);
+        }
+        /// <summary>
+        /// Generates a random 3D point within a spherical volume defined by a center and radius.
+        /// </summary>
+        /// <param name="center">The center of the sphere in 3D space.</param>
+        /// <param name="radius">The radius of the sphere, determining the distance from the center to the generated point.</param>
+        /// <returns>A randomly generated Vector3 point within the specified sphere.</returns>
+        public static Vector3 GetRandomPointInSphere(this Vector3 center, float radius)
+        {
+            float randomRadius = Random.Range(0f, radius);
+            float randomTheta = Random.Range(0f, 2f * Mathf.PI);
+            float randomPhi = Random.Range(0f, Mathf.PI);            
+
+            float x = center.x + randomRadius * Mathf.Sin(randomPhi) * Mathf.Cos(randomTheta);
+            float y = center.y + randomRadius * Mathf.Sin(randomPhi) * Mathf.Sin(randomTheta);
+            float z = center.z + randomRadius * Mathf.Cos(randomPhi);
+
+            return new Vector3(x, y, z);
+        }
+
     }
 }
