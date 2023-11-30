303c760575f2e4728df81cf4ac98c53070082d8e
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2023-11-30 14:23:12 +0530

    Documentated Easing Extension

--- Assets/Extensions/Script/EasingExtension.cs
+++ Assets/Extensions/Script/EasingExtension.cs
@@ -4,29 +4,53 @@ namespace Extension
 {
     public static class EasingExtensions
     {
+        /// <summary>
+        /// Linear interpolation between 0 and a specified maximum value.
+        /// </summary>
+        /// <param name="t">The interpolation factor (usually between 0 and 1).</param>
+        /// <param name="max">The maximum value to interpolate to (default is 1).</param>
+        /// <returns>The result of linear interpolation between 0 and the specified maximum value.</returns>
         public static float LerpLinear(this float t, float max = 1f)
         {
             float result = t * max;
             return (result > max) ? max : result;
         }
 
+        /// <summary>
+        /// Ease-in quadratic interpolation between 0 and a specified maximum value.
+        /// </summary>
+        /// <param name="t">The interpolation factor (usually between 0 and 1).</param>
+        /// <param name="max">The maximum value to interpolate to (default is 1).</param>
+        /// <returns>The result of ease-in quadratic interpolation between 0 and the specified maximum value.</returns>
         public static float LerpEaseInQuad(this float t, float max = 1f)
         {
             float result = t * t * max;
             return (result > max) ? max : result;
         }
+
+        /// <summary>
+        /// Ease-out quadratic interpolation between 0 and a specified maximum value.
+        /// </summary>
+        /// <param name="t">The interpolation factor (usually between 0 and 1).</param>
+        /// <param name="max">The maximum value to interpolate to (default is 1).</param>
+        /// <returns>The result of ease-out quadratic interpolation between 0 and the specified maximum value.</returns>
         public static float LerpEaseOutQuad(this float t, float max = 1f)
         {
             float result = max - (max - t * t * max);
             return (result > max) ? max : result;
         }
 
+        /// <summary>
+        /// Ease-in-out quadratic interpolation between 0 and a specified maximum value.
+        /// </summary>
+        /// <param name="t">The interpolation factor (usually between 0 and 1).</param>
+        /// <param name="max">The maximum value to interpolate to (default is 1).</param>
+        /// <returns>The result of ease-in-out quadratic interpolation between 0 and the specified maximum value.</returns>
         public static float LerpEaseInOutQuad(this float t, float max = 1f)
         {
-            float result = (t < 0.5) ? t * t * max * 2 : max - Mathf.Pow(max * 2 * t - 2, 2) / 2;
+            float result = (t < 0.5f) ? t * t * max * 2 : max - Mathf.Pow(max * 2 * t - 2, 2) / 2;
             return (result > max) ? max : result;
         }
-
 
     }
 
